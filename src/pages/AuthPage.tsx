8e99218a88f789df25a460f0fa9cf42bc625931f
Manivel Manoharan <38492388+itsmemanivel@users.noreply.github.com>
2023-12-16 10:37:33 +0000

    feat: insta login

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,112 +1,25 @@
 // AuthPage.tsx
-import React, { useState } from 'react';
 import styles from '../assets/stylesheets/AuthPage.module.css';
+import instagramLogo from '../assets/images/Instagram_icon.png'; // Make sure you have an Instagram logo image
 
-// Define types for your form states if needed, for example:
-type LoginFormState = {
-  username: string;
-  password: string;
-};
-
-type RegisterFormState = {
-  username: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
 
 const AuthPage: React.FC = () => {
-  const [isLogin, setIsLogin] = useState<boolean>(true); // Toggle state between login and register
-  const [loginForm, setLoginForm] = useState<LoginFormState>({ username: '', password: '' });
-  const [registerForm, setRegisterForm] = useState<RegisterFormState>({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
+  const instagramClientId = '168261799683361';
+  const instagramRedirectUri = 'https://socialsizzle.herokuapp.com/auth/';
+  const instagramScope = 'user_profile,user_media';
+  const instagramAuthUrl = `https://api.instagram.com/oauth/authorize?client_id=${instagramClientId}&redirect_uri=${instagramRedirectUri}&scope=${instagramScope}&response_type=code`;
 
-  // Handlers for form inputs
-  const handleLoginInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginForm({ ...loginForm, [event.target.name]: event.target.value });
-  };
-
-  const handleRegisterInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRegisterForm({ ...registerForm, [event.target.name]: event.target.value });
-  };
-
-  // Mock submit handlers
-  const handleLoginSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Handle login logic here
-  };
-
-  const handleRegisterSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Handle registration logic here
-  };
 
   return (
     <div className={styles.container}>
       <div className={styles.form}>
-        {isLogin ? (
-          <form onSubmit={handleLoginSubmit} >
-            <input
-              type="text"
-              name="username"
-              value={loginForm.username}
-              onChange={handleLoginInputChange}
-              placeholder="Username"
-              className={styles.input}
-            />
-            <input
-              type="password"
-              name="password"
-              value={loginForm.password}
-              onChange={handleLoginInputChange}
-              placeholder="Password"
-              className={styles.input}
-            />
-            <button type="submit" className={styles.button}>Login</button>
-          </form>
-        ) : (
-          <form onSubmit={handleRegisterSubmit} >
-            <input
-              type="text"
-              name="username"
-              value={registerForm.username}
-              onChange={handleRegisterInputChange}
-              placeholder="Username"
-              className={styles.input}
-            />
-            <input
-              type="email"
-              name="email"
-              value={registerForm.email}
-              onChange={handleRegisterInputChange}
-              placeholder="Email"
-              className={styles.input}
-            />
-            <input
-              type="password"
-              name="password"
-              value={registerForm.password}
-              onChange={handleRegisterInputChange}
-              placeholder="Password"
-              className={styles.input}
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              value={registerForm.confirmPassword}
-              onChange={handleRegisterInputChange}
-              placeholder="Confirm Password"
-              className={styles.input}
-            />
-            <button type="submit" className={styles.button}>Register</button>
-          </form>
-        )}
-        <button onClick={() => setIsLogin(!isLogin)} className={styles.toggleButton}>
-          {isLogin ? 'Need to create an account?' : 'Already have an account?'}
+        <button onClick={() => window.location.href = instagramAuthUrl} className={styles.instagramAuthButton}>
+          <div>
+            <img src={instagramLogo} alt="Instagram logo" className={styles.logo} />
+          </div>
+          <div>
+            <span>Sign in with Instagram</span>
+          </div>
         </button>
       </div>
     </div>
